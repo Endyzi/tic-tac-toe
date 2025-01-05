bcbe595c44770a9d02b2dd68c13dc0a1009e1ee6
Endyzi <private_nilhamn@hotmail.com>
2025-01-05 14:59:36 +0100

    cleaned up code

--- app/library/Controllers/LeaderboardController.php
+++ app/library/Controllers/LeaderboardController.php
@@ -9,40 +9,26 @@ use App\Views\LeaderboardView;
 class LeaderboardController implements ControllerInterface
 {
 
-    public function indexAction(): LeaderboardView {
-
+    public function indexAction(): LeaderboardView
+    {
+        //functions for leaderboard
         $view = new LeaderboardView();
 
         $playersTable = new PlayersTable();
         $view->players = $playersTable->getTopPlayers();
         $view->totalPlayers = $playersTable->getTotalPlayers();
 
-        //$players = (new PlayersTable())->getAllPlayers();
-       
-        //$view->players = $players;
+
+
+
         return $view;
     }
-    /*public function indexAction(): AbstractView
-    {
-        $view = new LeaderboardView();
-
-        // Todo: redo this crap!
-        $players = (new PlayersTable())->getLeaders(10);
-        $view->players = $players;
-
-        return $view;
-    }*/
 
 
-/*senaste
-    public function getAllPlayersAction(): AbstractView {
-        $view = new LeaderboardView(); // Du kan använda samma vy
-        $players = (new PlayersTable())->getAllPlayers();
-        $view->players = $players; // Tilldela spelarna till vyn
-        return $view;
-    }*/
 
 
-    
+
+
+
 
 }
