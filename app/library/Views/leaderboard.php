342bdfe7e6c62b262aae6ecce622b11d4c0e6fbf
Endyzi <private_nilhamn@hotmail.com>
2025-01-05 14:16:41 +0100

    added data from player table to leaderboard,

--- app/library/Views/leaderboard.php
+++ app/library/Views/leaderboard.php
@@ -8,5 +8,28 @@
 ?>
 
 <div class="row content-container col-xs-12">
-    <?php var_export($this->players) ?>
+   
 </div>
+<!--displaying data from the players table, will change again soon for cleaner look, need to sort based on highest ranking.   -->
+<table>
+    <thead>
+        <tr>
+            <th>Player ID</th>
+            <th>Player Name</th>
+            <th>Grid Size</th>
+            <th>Play Time (Seconds)</th>
+            <th>Created Time</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($this->players as $player): ?>
+            <tr>
+                <td><?= htmlspecialchars($player['player_id'] ?? '') ?></td>
+                <td><?= htmlspecialchars($player['name'] ?? '') ?></td>
+                <td><?= htmlspecialchars($player['grid_size'] ?? '') ?></td>
+                <td><?= htmlspecialchars($player['play_time_seconds'] ?? '') ?></td>
+                <td><?= htmlspecialchars($player['ctime'] ?? '') ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
