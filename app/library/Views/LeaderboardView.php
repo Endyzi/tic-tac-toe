5c65a0a85a9811d18b02b23694876adb190c7c40
Endyzi <private_nilhamn@hotmail.com>
2025-01-06 11:43:21 +0100

    fix for warning, deprecated dynamic properties

--- app/library/Views/LeaderboardView.php
+++ app/library/Views/LeaderboardView.php
@@ -6,6 +6,8 @@ class LeaderboardView extends AbstractView
 {
     /** @var array[][] */
     public array $players = [];
+    //had to add this public int due to warning about deprecated dynamic properties, apparently dynamic properties cannot be used in version php 8.2 and newer.
+    public int $totalPlayers = 0;
 
     public function __construct()
     {
