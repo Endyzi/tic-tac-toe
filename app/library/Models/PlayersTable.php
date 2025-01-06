96f3e968f34866175fcacfa3f445165ea6c6601e
Endyzi <private_nilhamn@hotmail.com>
2025-01-06 11:43:49 +0100

    cleaned up code, removed unused functions.

--- app/library/Models/PlayersTable.php
+++ app/library/Models/PlayersTable.php
@@ -21,12 +21,6 @@ class PlayersTable extends AbstractTable
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    //will probably remove this function or change it 
-    public function getAllPlayers(): array
-    {
-        $stmt = $this->pdo->query("SELECT * FROM {$this->table}");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
     
     //fetches top players based on grid and playtime
     public function getTopPlayers(): array
@@ -62,46 +56,4 @@ class PlayersTable extends AbstractTable
         return (int)$this->pdo->lastInsertId();
     }
 
-    //fetches all players, will use this for leaderboard, unused
-    /*public function  getAllPlayers(): array
-    {
-        $stmt = $this->pdo->query('SELECT * FROM players');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-
-    /*public function getLeaders(int $gridSize): array
-    {
-        $stmt = $this->pdo->prepare("
-            
-                SELECT
-                    name,
-                    play_time_seconds,
-                    grid_size
-                FROM players
-                WHERE
-                    grid_size = :grid_size
-            ");
-            
-        
-        $stmt->execute([':grid_size' => $gridSize]);
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-
-    /*public function addRow(string $name, int $gridSize, int $playTimeSeconds, string $date): void
-    {
-        $stmt = $this->pdo->prepare("
-            
-                INSERT INTO players
-                    (name, grid_size, play_time_seconds, ctime)
-                VALUE
-                    (:name, :grid_size, :play_time_seconds, :date)
-            ");
-            $stmt->execute([
-                ':name' => $name,
-                ':grid_size' => $gridSize,
-                ':play_time_seconds' => $playTimeSeconds,
-                ':date' => $date,
-            ]);
-        
-    }*/
 }
